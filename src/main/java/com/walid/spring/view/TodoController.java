75156355a6fecf796732692c1a89cf055a199fc0
Walid Moustafa <wmoustafa@shelde.com>
2018-06-30 22:39:44 +1000

    add Swagger API documentation

--- src/main/java/com/walid/spring/view/TodoController.java
+++ src/main/java/com/walid/spring/view/TodoController.java
@@ -2,6 +2,8 @@ package com.walid.spring.view;
 
 import com.walid.spring.model.Todo;
 import com.walid.spring.service.TodoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,24 +13,29 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController("/users")
+@Api(value = "TODO API", description = "TODO CRUD Endpoints API")
+@RestController
+@RequestMapping("/users")
 public class TodoController {
 
     @Autowired
     TodoService todoService;
 
-    @GetMapping("/users/{name}/todos")
+    @ApiOperation(value = "Retrieves TODOs for a specific user", response = List.class)
+    @GetMapping(value = "/{name}/todos", produces = "application/json")
     public List<Todo> retrieveTodos(@PathVariable String name) {
         return todoService.retrieveTodos(name);
     }
 
-    @GetMapping(path = "/users/{name}/todos/{id}")
+    @ApiOperation(value = "Retrieves a specific TODO", response = Todo.class)
+    @GetMapping(path = "/{name}/todos/{id}")
     public Todo retrieveTodo(@PathVariable String name, @PathVariable int id) {
         return todoService.retrieveTodo(id);
     }
 
-    @PostMapping("/users/{name}/todos")
-    ResponseEntity<?> add(@PathVariable String name, @Valid @RequestBody Todo todo) {
+    @ApiOperation(value = "Adds a new TODO", response = Todo.class)
+    @PostMapping("/{name}/todos")
+    ResponseEntity add(@PathVariable String name, @Valid @RequestBody Todo todo) {
         Todo createdTodo = todoService.addTodo(name, todo.getDesc(), todo.getTargetDate(), todo.isDone());
         if (createdTodo == null) {
             return ResponseEntity.noContent().build();
